1fa3a3343203bd5e91e9f2f660bfaefe5d30272f
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2023-06-06 01:19:31 +0530

    learn: Configuring Widgets and Understanding Objects

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 7, 255, 205),
         body: Center(
           child: Text('Hello Cool'),
         ),
