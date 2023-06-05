e794610f4d21c74d7fd865b88f488eeee45e744b
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2023-06-06 01:33:01 +0530

    learn: working with configuration objects(non-widget objects)

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 7, 255, 205),
-        body: Center(
-          child: Text('Hello Cool'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: Colors.accents,
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello Cool'),
+          ),
         ),
       ),
     ),
